f894d9f18eb1c4a09c692891935c8261db53d9c1
NatyBlass <natypaz12345@gmail.com>
2024-04-20 18:24:55 -0300

    Punto 5 Realizado

--- tp3_3.c
+++ tp3_3.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 5
+
 
 int main()
 {
 
     char buff[50];
-    char **v = malloc(sizeof(char*) * MAX);
-    int longitud;
+    char **v; //malloc(sizeof(char*) * MAX);
+    int longitud, cant;
 
-    for (int i = 0; i < MAX; i++)
+    printf("Cantidad de nombres que desea ingresar:\n");
+    scanf("%d", &cant);
+    v = malloc(sizeof(char*) * cant);
+
+    for (int i = 0; i < cant ; i++)
     {
-        puts("Ingrese un Nombre: ");
-        gets(buff);
+        printf("Ingrese un Nombre:\n");
+        scanf("%s",&buff);
         longitud = strlen(buff) + 1;
         v[i] = malloc(sizeof(char) * longitud);
         strcpy(v[i], buff);
     }
     
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < cant; i++)
     {
         printf("Nombre %d: ", i+1);
         puts(v[i]);
     }
     
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < cant; i++)
     {
         free(v[i]);
     }
